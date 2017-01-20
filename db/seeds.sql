eddfedabb31066744f652332ef56c44a200960bd
Joshua Ojeda <Josh@Admins-MacBook-Pro-3.local>
2017-01-20 13:38:18 -0800

    hw

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+
+INSERT INTO burger(id,burger_name,Devoured,date)VALUES(NULL,"hamburger",true,NULL);
+INSERT INTO burger(id,burger_name,Devoured,date)VALUES(NULL,"cheese burger",false,NULL);
+INSERT INTO burger(id,burger_name,Devoured,date)VALUES(NULL,"veggie burger",true,NULL);
+INSERT INTO burger(id,burger_name,Devoured,date)VALUES(NULL,"double cheese burger",false,NULL);
---
+++ models/burger.js
@@ -0,0 +1,14 @@
+
+
+var orm = require('../config/orm.js');
+
+var burger = {
+	all: function(cb){
+		orm.selectAll('burger',function(res){
+			cb(res)
+
+		});
+	}
+};
+
+module.exports = burger;
